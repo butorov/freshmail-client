a42caef735c4923c78dc3e1ac4c4b1b1cc129bed
V.Butorov <v.butorov@leadsor.com>
2015-08-06 04:57:20 +0700

    Added base for tests.

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
+import sys
+
 from distutils.core import setup
+from setuptools.command.test import test
+
+
+class PyTest(test):
+    user_options = [('pytest-args=', 'a', 'Arguments to pass into py.test')]
+
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 requirements = [
@@ -22,4 +43,5 @@ setup(
     description='Freshmail client for Python',
     install_requires=requirements,
     tests_require=test_requirements,
+    cmdclass={'test': PyTest},
 )
